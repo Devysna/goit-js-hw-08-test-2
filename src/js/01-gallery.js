5b1c254ecc4b98c3460aff6f519dc414397c0838
Devysna <yashturman@gmail.com>
2023-03-06 18:35:40 +0200

    Загальні налаштування

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -4,3 +4,31 @@ import { galleryItems } from './gallery-items';
 
 console.log(galleryItems);
 
+import SimpleLightbox from 'simplelightbox';
+
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const gallery = document.querySelector('.gallery');
+
+
+const newGalleryItems = [];
+
+const galleryMarkup = generateGalleryMarkup(galleryItems);
+gallery.insertAdjacentHTML('beforeend', galleryMarkup);
+
+function generateGalleryMarkup(galleryItems) {
+  return galleryItems.map((galleryItem) => `
+    <div class="gallery__item">
+      <a class="gallery__link" href="${galleryItem.original}">
+        <img class="gallery__image" 
+             src="${galleryItem.preview}"
+             data-source="${galleryItem.original}"
+             alt="${galleryItem.description}" />
+      </a>
+    </div>
+  `).join('');
+};
+
+new SimpleLightbox('.gallery a', {
+	captionDelay: 250
+});
